7cf22da471f33a79e26e972ce107fa2510e5b18d
Vijayita Gumber <vijayitagumber@Vijayitas-MacBook-Pro.local>
2024-08-01 19:23:00 -0700

    Day 3 progress.

---
+++ 005-validPalindrome.java
@@ -0,0 +1,46 @@
+/*
+QNo:125 Valid Palindrome
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+*/
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        String st = s.toLowerCase();
+        st = st.replaceAll("[^a-zA-Z0-9]", "");
+        int i = 0;
+        int j = st.length()-1;
+        while(i<=j){
+            if(st.charAt(i) != st.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
+
+/*
+Time Complexity : O(N)
+*/
